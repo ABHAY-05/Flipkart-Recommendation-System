2baf08f945e8aeac3fb9b231d52d22bdc157cbe7
ABHAY-05 <83771160+ABHAY-05@users.noreply.github.com>
2023-08-30 00:38:49 +0530

    Update axios.js

--- client/src/adapters/axios.js
+++ client/src/adapters/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL:"https://flipkart-recommendation-api.onrender.com/api",
+  baseURL:"http://localhost:8000/api",
 });
 
 axiosInstance.interceptors.response.use(
