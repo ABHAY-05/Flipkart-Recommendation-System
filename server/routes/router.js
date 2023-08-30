1f538769af641474e722365fb2990ac059f05bbd
ABHAY-05 <83771160+ABHAY-05@users.noreply.github.com>
2023-08-30 09:07:20 +0530

    Update router.js

--- server/routes/router.js
+++ server/routes/router.js
@@ -49,7 +49,7 @@ const {
 const router = express.Router();
 
 router.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://flipkart-recommendation-system.vercel.app/");
+  res.header("Access-Control-Allow-Origin", "https://flipkart-recommendation-system.vercel.app");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
