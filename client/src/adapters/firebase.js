a5092b1e5aa5341ed461778de1e9f95f9ef55381
ABHAY-05 <83771160+ABHAY-05@users.noreply.github.com>
2023-08-30 09:13:37 +0530

    Update firebase.js

--- client/src/adapters/firebase.js
+++ client/src/adapters/firebase.js
@@ -1,5 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import * as dotenv from 'dotenv';
+
+dotenv.config({ path: '../../.env' })
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
